9b3a04973ea2c9f21d31390285b92b03d8e5ffbd
Maryam Darbari <maryam.darbari2000@gmail.com>
2025-12-10 22:29:49 +0330

    Add project files.

---
+++ DotNetFundamental0409/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetFundamental0409
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
