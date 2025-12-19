f43c5b1da48fe5190854e05497f19433359e550e
Maryam Darbari <maryam.darbari2000@gmail.com>
2025-12-19 19:29:55 +0330

    V1.2

--- DotNetFundamental0409/Program.cs
+++ DotNetFundamental0409/Program.cs
@@ -7,17 +7,23 @@
             Console.WriteLine("What is your First Name");
             String Name = Console.ReadLine();
             Console.WriteLine("What is your Last Name");
-            String FamilyName=Console.ReadLine();
+            String FamilyName = Console.ReadLine();
+            String FullName = Name + " " + FamilyName;
             Console.WriteLine("How old are you?");
-                String Age = Console.ReadLine();
+            String AgeStr = Console.ReadLine();
+            int Age = int.Parse(AgeStr);
             Console.WriteLine("What is your phone number?");
-            String PhoneNumber = Console.ReadLine();
+            String PhoneNumberStr = Console.ReadLine();
+            int PhoneNumkber = int.Parse(PhoneNumberStr);
             Console.WriteLine("What is your card number?");
-                String CardNumber = Console.ReadLine();
+            String CardNumberStr = Console.ReadLine();
+            int CardNumber = int.Parse(CardNumberStr);
             Console.WriteLine("What is your national identification number?");
-                String ID = Console.ReadLine();
-            Console.WriteLine("Thanks "  +  Name);
+            String IDStr = Console.ReadLine();
+            int ID = int.Parse(IDStr);
+            Console.WriteLine("Thanks " + FullName);
             
+
         }
     }
 }
