75185cdc5d5e7a39f9b592da7e25449ed421f3f6
Maryam Darbari <maryam.darbari2000@gmail.com>
2026-02-10 20:57:20 +0330

    Task 1

---
+++ DotNetFundamental0409/Class2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetFundamental0409
+{
+    internal class Class2
+    {
+    }
+}
--- DotNetFundamental0409/Program.cs
+++ DotNetFundamental0409/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.Marshalling;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
 namespace DotNetFundamental0409
 {
@@ -7,88 +12,197 @@ namespace DotNetFundamental0409
         static void Main(string[] args)
 
         {
-
-            Console.WriteLine("What is your First Name");
-            String Name = Console.ReadLine();
-            Console.WriteLine("What is your Last Name");
-            String FamilyName = Console.ReadLine();
-            String FullName = Name + " " + FamilyName;
-            Console.WriteLine("How old are you?");
-            String AgeStr = Console.ReadLine();
-            int Age = int.Parse(AgeStr);
-            if (!(Age>=0 && Age<=100)) {
-                Console.WriteLine("Please enter a valid number!"); return;
-            }
-            Console.WriteLine("What is your phone number?");
-            String PhoneNumberStr = Console.ReadLine();
-            long PhoneNumber = long.Parse(PhoneNumberStr);//In Estefade ei nadare
-
-            if (PhoneNumberStr.Length == 11)
+            while (true)
             {
-
-
-            }
-            else
-            {
-                Console.WriteLine("Your phone number is invalid");
-                return;
-            }
-
-
-
-            string CardNumberStr = "";
-            while (!(CardNumberStr.Length == 16))
-            {
-                Console.WriteLine("What is your Card Number?");
-                CardNumberStr = Console.ReadLine();
-                if (!(CardNumberStr.Length == 16)) 
+                Console.WriteLine("What is your First Name");
+                String Name = Console.ReadLine();
+                if (HamashHarfBashe(Name) && NullNabashe(Name))
                 {
-                    Console.WriteLine("Please Enter a Valid Card Number");
-                    
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid name");
+                }
+            }
+
+            MagentaSubmitted();
+
+            while (true)
+            {
+                Console.WriteLine("What is your family name?");
+                String FamilyName = Console.ReadLine();
+                if (HamashHarfBashe(FamilyName) && NullNabashe(FamilyName))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid Family Name.");
+                }
+
+            }
+            MagentaSubmitted();
+
+
+            while (true)
+            {
+
+
+                Console.WriteLine("How old are you?");
+                String AgeStr = Console.ReadLine();
+
+                if (!(HamashAdadBashe(AgeStr) && NullNabashe(AgeStr)))
+                {
+                    Console.WriteLine("Please enter a valid number!");
+
+                }
+
+
+                else if ((AgeStr.Length > 2))
+                {
+                    Console.WriteLine("Please enter a valid age!");
 
                 }
                 else
                 {
-                    
+                    break;
+                }
+            }
+            MagentaSubmitted();
+            while (true)
+            {
+                Console.WriteLine("What is your phone number?");
+                String PhoneNumberStr = Console.ReadLine();
+
+
+                if (!(PhoneNumberStr.Length == 11 && HamashAdadBashe(PhoneNumberStr) && NullNabashe(PhoneNumberStr)))
+                {
+                    Console.WriteLine("Please enter a valid phone number");
+
+
+                }
+                else
+                {
+                    break;
+
+                }
+            }
+            MagentaSubmitted();
+
+            string CardNumberStr = "";
+            while (true)
+            {
+                Console.WriteLine("What is your Card Number?");
+                CardNumberStr = Console.ReadLine();
+                if (!(CardNumberStr.Length == 16 && HamashAdadBashe(CardNumberStr) && NullNabashe(CardNumberStr)))
+                {
+                    Console.WriteLine("Please Enter a Valid Card Number");
+
+
+                }
+                else
+                {
+                    break;
                 }
             }
 
-                
-            String IDStr = "";
+            MagentaSubmitted();
 
-            while (!(IDStr.Length <= 18 && IDStr.Length>=9))
+
+            while (true)
             {
+
                 Console.WriteLine("What is your national identification number?");
-                IDStr = Console.ReadLine();
-                if ((!(IDStr.Length <= 18 && IDStr.Length >= 9)))
+                string IDStr = Console.ReadLine();
+                if (!(IDStr.Length <= 18 && IDStr.Length >= 9&&HamashAdadBashe(IDStr)&&NullNabashe(IDStr)))
                 {
-                    Console.WriteLine("Please enter a valid number");
+                    Console.WriteLine("Please enter a valid identification number");
                 }
                 else if (IDStr.Length == 9)
                 {
                     Console.WriteLine("United States");
+                    
+                   Ending();
+                    break;
+
                 }
                 else if (IDStr.Length == 12)
                 {
                     Console.WriteLine("India");
+                    Ending();
+                    break;
                 }
                 else if (IDStr.Length == 11)
                 {
                     Console.WriteLine("Brazil");
+                    Ending();
+                    break;
                 }
                 else if (IDStr.Length == 9)
                 {
                     Console.WriteLine("United Kingdom");
+                    Ending();
+                    break;
                 }
                 else
                 {
-                    Console.WriteLine("Unknown");
+                    Ending();
+                    break;
                 }
             }
-          
-            Console.WriteLine("Thanks" + " " + FullName);
+           
+        }
+                
+
+            
+        
+        public static bool HamashHarfBashe(string N)
+        {
+            foreach (char m in N)
+            {
+                if (char.IsLetter(m))
+                {
+                    return true;
+                }
+                
+            }
+            return false;
+        }
+        public static bool HamashAdadBashe(string N)
+        {
+            foreach (char m in N)
+            {
+                if (char.IsDigit(m))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool NullNabashe(string N)
+        {
+            if (string.IsNullOrEmpty(N))
+            {
+                return false;
+            }
+            return true;
 
 
         }
+        public static void MagentaSubmitted()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Submitted");
+            Console.ResetColor();
+        }
+
+        public static void Ending() { 
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Thanks for your submission");
+            Console.ResetColor();
+        }
+
+
     }
 }
